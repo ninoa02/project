05f549b91237cce80a0460ebab27482b4423ca99
Lee <sangyung.lee.kr@gmail.com>
2016-11-21 20:36:02 +0900

    no message

--- app/src/main/java/com/example/sangy/arduinonandroid/AlarmActivity.java
+++ app/src/main/java/com/example/sangy/arduinonandroid/AlarmActivity.java
@@ -3,15 +3,35 @@ package com.example.sangy.arduinonandroid;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TimePicker;
 
 /**
  * Created by sangy on 2016-11-13.
  */
 
 public class AlarmActivity extends AppCompatActivity {
+    TimePicker timePickerStart;
+    TimePicker timePickerStop;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+        timePickerStart = (TimePicker)findViewById(R.id.timePickerStart);
+        timePickerStop = (TimePicker)findViewById(R.id.timePickerStop);
 
+
+    }
+
+
+    public void release(View view){
+        int startHour = timePickerStart.getHour();
+        int startMinute = timePickerStart.getMinute();
+        int stopHour = timePickerStop.getHour();
+        int stopMinute = timePickerStop.getMinute();
+        finish();
+    }
+    public void setting(View view){
+        finish();
     }
 }
